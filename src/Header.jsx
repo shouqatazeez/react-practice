380f15ed248ff8f4fb082453d156679236eed64b
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-07-05 16:48:56 +0530

    add search to the nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,6 +6,13 @@ const Header = () => {
     <div>
       <div className="flex border-1 to-black  pr-32">
         <img src={logo} className="h-24" />
+        <div className="flex-1 flex justify-center px-16 h-12 m-7  ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="border px-4 py-1 w-full max-w-md  rounded"
+          />
+        </div>
         <div className="flex items-center space-x-16 ml-auto ">
           <a href="/">Home</a>
           <a href="/">About Us</a>
