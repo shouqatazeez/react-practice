ea7b364f20cfeb4e312eb04d6f49ed6a2851af3f
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-30 13:55:08 +0530

    add the app file with the config file

--- Tabformcomponent/src/App.jsx
+++ Tabformcomponent/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Profile from "./components/Profile";
+import Interests from "./components/Interests";
+import Settings from "./components/Settings";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const data = [
+  {
+    name: "profile",
+    component: <Profile />,
+  },
+  {
+    name: "Interests",
+    component: <Interests />,
+  },
+  {
+    name: "Settings",
+    component: <Settings />,
+  },
+];
+const App = () => {
+  const [activeTab, SetActiveTab] = useState(0);
+  const ActiveTab = data[activeTab].component;
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col p-4">
+      <div className="flex  mb-1 gap-0.5">
+        {data.map((n, index) => (
+          <div
+            onClick={() => SetActiveTab(index)}
+            className="border p-2 cursor-pointer rounded"
+            key={index}
+          >
+            {n.name}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className=" border h-80 w-80 p-4 rounded">{ActiveTab} </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
