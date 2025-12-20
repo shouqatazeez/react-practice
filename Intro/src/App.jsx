6d93b0ff9b0a129423fbad0d187ad0ee964c4d58
Shouqat Azeez <228x1a05b7@khitguntur.ac.in>
2025-12-20 20:39:56 +0530

    work with a small assigment task

--- Intro/src/App.jsx
+++ Intro/src/App.jsx
@@ -4,14 +4,19 @@ import { useState } from "react";
 const App = () => {
   const [count, setCount] = useState(0);
   const addvalue = () => {
-    setCount(count + 1);
+    if (count < 20) {
+      setCount(count + 1);
+    }
   };
   const removevalue = () => {
-    setCount(count - 1);
+    if (count > 0) {
+      setCount(count - 1);
+    }
   };
   return (
     <div>
       <h1>Counter App value {count}</h1>
+
       <button onClick={addvalue}>Click me </button>
       <button onClick={removevalue}>Remove value</button>
     </div>
