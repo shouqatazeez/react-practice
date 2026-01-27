b0459a5dfb61316474515243df2cd6dae84b3905
Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-01-27 18:49:35 +0530

    removed the unwanted code from the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./Header";
+
 const App = () => {
-  let country = "India";
+  
   return (
-    <div className="text-center">
-      <Header count={country} />
+    <div >
+      
     </div>
   );
 };
